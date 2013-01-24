0d21910b99566754b08e53e64945eba4c3e72a9b
Nisha Sehn <nishasehn@gmail.com>
2013-01-23 20:06:41 -0800

    Dictionary loads.

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -3,16 +3,22 @@ require 'singleton'
 class Dictionary
   include Singleton
 
-  class << self
-    attr_accessor :words
+  class << self #opens up self's singleton class, so that methods can be redefined 
+                #for the current self object (which inside a class or module body
+                #is the class or module itself)
+                #class is Dictionary which opens up self
+                
+    attr_accessor :words #attribute accessor. words is the attribute (instance variable)
   end
 
   # We load the dictionary file into the @words class variable.
   # We only want to load the dictionary file if the @words class variable is empty.
   def self.load_dictionary
-
-    File.open("/usr/share/dict/words", "r").each_line do |line|
-
+    if !@words.kind_of?(Array) #check to see if @words is NOT an array
+      @words = [] #if it is not an array, make it an empty array
+      File.open("/usr/share/dict/words", "r").each_line do |line|
+        @words.push(line)
+      end
     end
   end
 
